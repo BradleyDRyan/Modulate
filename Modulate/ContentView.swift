57664ddd5d387bdbf3ffff27d2298ed9154242d4
BradleyUeno <52720930+BradleyUeno@users.noreply.github.com>
2020-05-07 06:21:42 -0500

    Initial Commit

---
+++ Modulate/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Modulate
+//
+//  Created by Bradley Ryan on 5/7/20.
+//  Copyright © 2020 Bradley Ryan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
